f3185ce077d00c9a7dc9f0b741f4d97bd4097405
minhi0449 <minhi0449@gmail.com>
2025-02-03 19:01:13 +0900

    애쁠페이 계좌생성

---
+++ src/main/java/com/aplus/aplusmarket/mapper/pay/PayMapper.java
@@ -0,0 +1,9 @@
+package com.aplus.aplusmarket.mapper.pay;
+
+/*
+ *  2025.02.03 (월) 김민희 -
+ *
+ * */
+
+public class PayMapper {
+}
---
+++ src/main/java/com/aplus/aplusmarket/service/ChargeService.java
@@ -0,0 +1,15 @@
+package com.aplus.aplusmarket.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+ *  2025.02.03 (월) 김민희 - 애쁠페이 [충전] 계좌생성, 충전, 잔액조회
+ *                         트랜잭션 처리, 보안검증
+ *
+ * */
+@Service
+@Transactional
+public class ChargeService {
+}
