befccb81367adfd06f80873011539a91ba20f69f
minhi0449 <minhi0449@gmail.com>
2025-02-03 19:01:13 +0900

    애쁠페이 계좌생성

---
+++ src/main/java/com/aplus/aplusmarket/entity/pay/AccoutHistory.java
@@ -0,0 +1,28 @@
+//package com.aplus.aplusmarket.entity.pay;
+
+import lombok.*;
+
+/*
+ *  2025.02.03 (월) 김민희 - 🍎 애쁠페이 [거래 내역]
+ *
+ *  [테이블명] : tb_account_history
+ *  [거래유형]
+ *  • Charge: 충전
+ *  • Pay: 결제
+ *  • Transfer: 이체
+ *  • Withdraw: 출금 (w)
+ *  • Deposit: 입금 (d)
+ * */
+
+//@Getter
+//@Setter
+//@ToString
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class AccoutHistory {
+//
+//
+//
+//
+//}
