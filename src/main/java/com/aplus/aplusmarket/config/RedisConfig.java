7f566e57e50439f9503fb67f97924d33498cc69a
hajhi7899 <hajhi7899@gmail.com>
2025-02-27 21:45:44 +0900

    update redisConfig

--- src/main/java/com/aplus/aplusmarket/config/RedisConfig.java
+++ src/main/java/com/aplus/aplusmarket/config/RedisConfig.java
@@ -24,14 +24,6 @@ public class RedisConfig {
     }
 
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return template;
-    }
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory) {
